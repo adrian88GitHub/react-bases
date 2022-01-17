d42bc69dfdca32d554facef7de6560451fe5eaac
adrian <adrian881228@gmail.com>
2022-01-17 20:32:40 -0300

    init

---
+++ src/counter-reducer/CounterReducer.tsx
@@ -0,0 +1,26 @@
+import { useReducer } from "react";
+import * as actions from "./actions/actions";
+import { CounterState } from "./interface/interface";
+import { counterReducer } from "./state/counterReducer";
+
+const INITIAL_STATE: CounterState = {
+  counter: 0,
+  previous: 0,
+  changes: 0,
+};
+
+export const CounterReducerComponent = () => {
+  const [counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE);
+  const increaseBy = (value: number) => dispatch(actions.doIncreaseBy(value));
+  const handleReset = () => dispatch(actions.doReset());
+  return (
+    <>
+      <h1>Counter Reducer Segmentado</h1>
+      <pre>{JSON.stringify(counterState, null, 2)}</pre>
+      <button onClick={() => increaseBy(1)}>+1</button>
+      <button onClick={() => increaseBy(5)}>+5</button>
+      <button onClick={() => increaseBy(10)}>+10</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  );
+};
